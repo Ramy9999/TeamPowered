ceca11eb933b80a24bac858847805fc7a2b81e75
Ramy <ramysarraf93@gmail.com>
2016-12-24 15:17:42 +0300

    Comicbook Git first commit

---
+++ ComicBookGallery-MVC-ASP/ComicBookGallery-MVC-ASP/Controllers/ComicBookController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery_MVC_ASP.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        public ActionResult Detail ()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return Content ("Hello this is the comic book's controller!");
+
+
+        }
+    }
+}
---
+++ ComicBookGallery-MVC-ASP/ComicBookGallery-MVC-ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ComicBookGallery_MVC_ASP.MvcApplication" Language="C#" %>
